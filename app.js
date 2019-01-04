45217da6e929ea1f1c046ffcbc84137868d76a15
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-04 01:11:42 +0000

    Used function expressions, callFunction

--- app.js
+++ app.js
@@ -1,35 +1,29 @@
-console.log(__dirname);
-console.log(__filename);
-
-var time = 0;
-
-setInterval(function(){ //interval. A clock breaks time down into intervals of seconds, minutes, and hours. An interval is a distinct measure of time or the physical or temporal distance between two things.
-    time += 2;
-    console.log(time + ' seconds have passed');
-}, 2000);
+function callFunction(fun){
+    fun();
+}
 
 
+//normal function statement 
 
+// function sayHi(){
+//     console.log('hi');
+// }
+// sayHi();
 
+//function expression (nameless function) ... works with asyncronous 
 
+var sayBye = function(){
+    console.log('bye');
+    
+};
 
-
-
-
-
-
-
-
+callFunction(sayBye);
 
 
 
 
 
-// var fs = require('fs');
 
-// var file = fs.readFile('input.txt','utf8',function(err,data){
-//     fs.writeFile('writeme.txt',data);
-//     console.log(data);
-// });
 
-// console.log('This is an instruction outside the sync file system.');
+
+
