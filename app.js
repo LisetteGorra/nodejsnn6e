e68b9ac80a56101573e2a3fae3a4179ed8001a73
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-04 00:49:23 +0000

    Used setInterval, __dirname, __filename

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-var fs = require('fs');
+console.log(__dirname);
+console.log(__filename);
 
-var file = fs.readFile('input.txt','utf8',function(err,data){
-    fs.writeFile('writeme.txt',data);
-    console.log(data);
-});
+var time = 0;
 
-console.log('This is an instruction outside the sync file system.');
+setInterval(function(){ //interval. A clock breaks time down into intervals of seconds, minutes, and hours. An interval is a distinct measure of time or the physical or temporal distance between two things.
+    time += 2;
+    console.log(time + ' seconds have passed');
+}, 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fs = require('fs');
+
+// var file = fs.readFile('input.txt','utf8',function(err,data){
+//     fs.writeFile('writeme.txt',data);
+//     console.log(data);
+// });
+
+// console.log('This is an instruction outside the sync file system.');
