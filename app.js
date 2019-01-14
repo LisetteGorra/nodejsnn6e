17e8b0612d01698e9ae7e0fd416425ec584ecb9d
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-14 03:59:10 +0000

    Readable Streams, used fs.createReadStream , chunk

--- app.js
+++ app.js
@@ -1,30 +1,75 @@
+var http = require('http');
+
 var fs = require('fs');
-fs.unlink('./stuff/writeME.txt', function(){
-    
-fs.rmdir('stuff');
+
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+
+myReadStream.on('data', function(chunk){
+    console.log('new chunk received:');
+    console.log(chunk);
 });
 
 
 
 
-// fs.mkdirSync('stuff');
-// fs.rmdirSync('stuff');
 
 
 
-// fs.mkdir('stuff', function(){
-//     fs.readFile('readME.txt', 'utf8', function(err, data){
-//     fs.writeFile('./stuff/writeME.txt', data);
-//     });
+
+
+
+// var server = http.createServer(function(req, res){
+//     console.log('request was made:' + req.url);
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hey you!');
+// });
+
+// server.listen(3000, '127.0.0.1');
+// console.log('hey, I am now listening to port 3000');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fs = require('fs');
+// fs.unlink('./stuff/writeME.txt', function(){
+    
+// fs.rmdir('stuff');
 // });
 
 
 
 
+// // fs.mkdirSync('stuff');
+// // fs.rmdirSync('stuff');
+
+
+
+// // fs.mkdir('stuff', function(){
+// //     fs.readFile('readME.txt', 'utf8', function(err, data){
+// //     fs.writeFile('./stuff/writeME.txt', data);
+// //     });
+// // });
 
 
 
 
 
 
-// fs.unlink('writeME.txt');
+
+
+
+
+// // fs.unlink('writeME.txt');
