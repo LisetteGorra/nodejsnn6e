112d44f73c9b1c354b618861319750e32007efce
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-15 19:39:20 +0000

    Basic routing, created routes for different key words and 404 error page

--- app.js
+++ app.js
@@ -9,14 +9,30 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     console.log('request was made:' + req.url);
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    var myObj = {
-        name: 'pip',
-        job: 'dragon',
-        age: 22
-    };
-    res.end(JSON.stringify(myObj));
-});   
+    if(req.url === '/home' || req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if(req.irl === '/contact'){
+      fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    }else if(req.url === '/api/ninjas'){
+        var ninjas = [{name: 'Beanie', age: 33}, {name: 'smitty, age: 30'}];
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(ninjas));
+    } else{
+       res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+
+    }  
+
+}); 
+
+    // var myObj = {
+    //     name: 'pip',
+    //     job: 'dragon',
+    //     age: 22
+    // };
+    // res.end("I'm hungry");
+  
    
     // var myReadStream = fs.createReadStream(__dirname + '/index.txt', 'utf8');
     // myReadStream.pipe(res);
