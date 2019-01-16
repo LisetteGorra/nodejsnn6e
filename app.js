ec977207f4f36c2a3b8aaf87c64963d5df7e83e5
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-16 17:29:35 +0000

    Used middleware & static files, linking route to directory where it is stored using epress.static, /assets, code app.use

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets',express.static('assets'));
 
-app.get('/', function(req, res){
-  res.render('Welcome to the homepage'); 
-});
+// function(req, res, next){
+//     console.log(req.url);  
+//     next();
+// });
 
 app.get('/', function(req, res){
    res.render('index'); 
