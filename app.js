20a0bc94aad417b8c6e8e9f1bd5ddf12dfd6cc58
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-16 19:38:14 +0000

    Handling POST requests, used data.email/who/department , contact-seuccess

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
+
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 
 app.set('view engine', 'ejs');
 app.use('/assets',express.static('assets'));
-
-// function(req, res, next){
-//     console.log(req.url);  
-//     next();
-// });
 
 app.get('/', function(req, res){
    res.render('index'); 
 });
 
 app.get('/contact', function(req, res){
-   res.render('contact'); 
+   console.log(req.query);
+   res.render('contact', {qs: req.query});
+});
+
+app.post('/contact', urlencodedParser, function(req, res){
+   console.log(req.body);
+   res.render('contact-success', {data: req.body});
 });
 
 app.get('/profile/:name' , function(req, res){
