1e9f86c8c48a2b606bb144e00f7993487d369d20
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-16 17:08:59 +0000

    Creating partial templates, adding partials code to all html files

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-// app.get('/', function(req, res){
-//   res.send('Welcome to the homepage'); 
-// });
 app.get('/', function(req, res){
-   res.sendFile(__dirname + '/index.html'); 
+  res.render('Welcome to the homepage'); 
+});
+
+app.get('/', function(req, res){
+   res.render('index'); 
 });
 
 app.get('/contact', function(req, res){
-   res.sendFile(__dirname + '/contact .html'); 
+   res.render('contact'); 
 });
 
 app.get('/profile/:name' , function(req, res){
