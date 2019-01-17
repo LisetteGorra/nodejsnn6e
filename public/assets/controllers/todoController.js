79dfc0fe4c3ffa4e1b690d3627a5621f36c806f4
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-17 19:12:58 +0000

    Fully functioning Todo app , used todoSchema , if statements for errors, and rendering

--- public/assets/controllers/todoController.js
+++ public/assets/controllers/todoController.js
@@ -10,31 +10,45 @@ var todoSchema = new mongoose.Schema({
 });
 
 var Todo = mongoose.model('Todo', todoSchema);
-var itemOne = Todo({item: 'bye chocolate'}).save(function(error){
-    if (error) throw error;
-    console.log('item saved');
-});
+// var itemOne = Todo({item: 'bye chocolate'}).save(function(error){
+//     if (error) throw error;
+//     console.log('item saved');
+// });
 
-var data = [{item: 'get eggs'}, {item: 'take out trash'}, {item:'do homework'}];
+//var data = [{item: 'get eggs'}, {item: 'take out trash'}, {item:'do homework'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-module.export = function(app){
+module.exports = function(app){
     
 app.get('/todo', function(req, res){
-
+    //get data from mongodb and pass it to the view 
+    Todo.find({}, function(error, data){
+        if(error) throw error;
+        res.render('todo', {todos: data});
+  });
     
 });
 
 app.post('/todo', urlencodedParser, function(req, res){
-    data.push(req.body);
+    //get data from the view and add it to mongodb
+    var newTodo = Todo(req.body).save(function(error, data){
+        if (error) throw error;
+        res.json(data);
+    });
     res.json(data);
 });
 
+    
+};
+
 app.delete('/todo/:item', function(req, res){
-    data = data.filter(function(todo){
-       return todo.item.replace(/ /g, '-')!== req.params.item; 
+    //delete the requested item from mongodb
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(error, data){
+        if (error) throw error;
+        res.json(data);
     });
-    res.joson(data);
+  
 });
 
-};
+});
+
