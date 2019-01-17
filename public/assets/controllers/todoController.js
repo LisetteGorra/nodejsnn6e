6aa240926bc6c2590cc29f69640bee1e52182283
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-17 18:44:57 +0000

    Created database on mlab, installed mongoose, connected to database with mlab, created Schema and new model, created new item and passed object through and saved it to data base

--- public/assets/controllers/todoController.js
+++ public/assets/controllers/todoController.js
@@ -1,4 +1,19 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect('mongod://<dbuser>:<dbpassword>@ds017195.mlab.com:17195/todo');
+
+//create a Schema - this is like ablueprint for our data
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'bye chocolate'}).save(function(error){
+    if (error) throw error;
+    console.log('item saved');
+});
 
 var data = [{item: 'get eggs'}, {item: 'take out trash'}, {item:'do homework'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
@@ -6,14 +21,7 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 module.export = function(app){
     
 app.get('/todo', function(req, res){
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    res.render('todo');
->>>>>>> version24.0
-=======
-    res.render('todo', {todos: data});
->>>>>>> version24.0
+
     
 });
 
