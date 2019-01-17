2a9b91104a2b2c629ee996f00757a4d37b25a348
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-17 16:33:48 +0000

    Adding and deleting items in a todo list, used .ajax, todo.item.replace , req.params.item , used for loop in todo html

--- public/assets/controllers/todoController.js
+++ public/assets/controllers/todoController.js
@@ -1,16 +1,25 @@
+var bodyParser = require('body-parser');
+
+var data = [{item: 'get eggs'}, {item: 'take out trash'}, {item:'do homework'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.export = function(app){
     
 app.get('/todo', function(req, res){
-    res.render('todo');
+    res.render('todo', {todos: data});
     
 });
 
-app.post('/todo', function(req, res){
-    
+app.post('/todo', urlencodedParser, function(req, res){
+    data.push(req.body);
+    res.json(data);
 });
 
-app.delete('/todo', function(req, res){
-    
+app.delete('/todo/:item', function(req, res){
+    data = data.filter(function(todo){
+       return todo.item.replace(/ /g, '-')!== req.params.item; 
+    });
+    res.joson(data);
 });
 
 };
